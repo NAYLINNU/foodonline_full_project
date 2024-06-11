8a97c8ce210d4cc409c8ac16168db94ae4a4fb01
NAYLINNU <anonymousyokel1@gmail.com>
2024-06-11 18:57:46 +0700

    Admin approval Vendor

--- vendor/models.py
+++ vendor/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from accounts.models import User,UserProfile
-# from accounts.utils import send_notification
+from accounts.utils import send_notification
 
 class Vendor(models.Model):
     user = models.OneToOneField(User, related_name = 'user', on_delete=models.CASCADE)
     user_profile = models.OneToOneField(UserProfile, related_name = 'userprofile',on_delete=models.CASCADE)
     vendor_name = models.CharField(max_length=100)
-    vendor_slug = models.SlugField(max_length=100, unique=True)  #Take care differently unique and blank between them 
+    # vendor_slug = models.SlugField(max_length=100, unique=True)  #Take care differently unique and blank between them 
     vendor_license = models.ImageField(upload_to='vendor/license')
     is_approved = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -16,25 +16,25 @@ class Vendor(models.Model):
         return self.vendor_name
     
     
-    #admin approval function
-    # def save(self,*args, **kwargs):
-    #     if self.pk is not None:
+    # admin approval function
+    def save(self,*args, **kwargs):
+        if self.pk is not None:
             
-    #         #Update
-    #         orig = Vendor.objects.get(pk=self.pk)
-    #         if orig.is_approved != self.is_approved:
-    #             mail_template = 'accounts/emails/admin_approval_email.html'
-    #             context = {
-    #                 'user': self.user,
-    #                 'is_approved':self.is_approved,
-    #             }
-    #             if self.is_approved == True:
-    #                 #Send notification mail
-    #                 mail_subject='Congratulation!, Your restaurant has been approved.'
-    #                 send_notification(mail_subject,mail_template,context)
+            #Update
+            orig = Vendor.objects.get(pk=self.pk)
+            if orig.is_approved != self.is_approved:
+                mail_template = 'accounts/emails/admin_approval_email.html'
+                context = {
+                    'user': self.user,
+                    'is_approved':self.is_approved,
+                }
+                if self.is_approved == True:
+                    #Send notification mail
+                    mail_subject='Congratulation!, Your restaurant has been approved.'
+                    send_notification(mail_subject,mail_template,context)
                 
-    #             else:                
-    #                 #Send notification mail
-    #                 mail_subject = "We're sorry! You are not eligible for publishing your food menu on our markplace"
-    #                 send_notification(mail_subject,mail_template,context)
-        # return super(Vendor,self).save(*args,**kwargs)
+                else:                
+                    #Send notification mail
+                    mail_subject = "We're sorry! You are not eligible for publishing your food menu on our markplace"
+                    send_notification(mail_subject,mail_template,context)
+        return super(Vendor,self).save(*args,**kwargs)
