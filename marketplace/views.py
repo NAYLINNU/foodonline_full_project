51109d53a887930a5be4e7c352c4d61ef0659b64
NAYLINNU <anonymousyokel1@gmail.com>
2024-07-26 23:21:40 +0700

    business hour

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
@@ -5,7 +6,7 @@ from .context_processors import get_cart_amounts, get_cart_counter
 
 from .models import Cart
 from menu.models import Category, FoodItem
-from vendor.models import Vendor
+from vendor.models import OpeningHour, Vendor
 from django.db.models import Prefetch
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
@@ -32,17 +33,25 @@ def vendor_detail(request, vendor_slug):
         )
     )
 
+    opening_hours = OpeningHour.objects.filter(vendor=vendor).order_by('day', 'from_hour')
+    
+    # Check current day's opening hours.
+    today_date = date.today()
+    today = today_date.isoweekday()
+    
+    current_opening_hours = OpeningHour.objects.filter(vendor=vendor, day=today)
     if request.user.is_authenticated:
         cart_items = Cart.objects.filter(user=request.user)
     else:
-        cart_items= None
-
+        cart_items = None
     context = {
         'vendor': vendor,
         'categories': categories,
         'cart_items': cart_items,
+        'opening_hours': opening_hours,
+        'current_opening_hours': current_opening_hours,
     }
-    return render(request,'marketplace/vendor_detail.html',context)
+    return render(request, 'marketplace/vendor_detail.html', context)
 
 
 def add_to_cart(request,food_id):
--- vendor/forms.py
+++ vendor/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Vendor
+from .models import Vendor,OpeningHour
 from accounts.validators import allow_image_only
 class VendorForm(forms.ModelForm):
     #The reason I use 'FileField' is because I use a custom validator ,image field not working
@@ -10,3 +10,9 @@ class VendorForm(forms.ModelForm):
     class Meta:
         model = Vendor
         fields = ['vendor_name','vendor_license']
+        
+        
+class OpeningHourForm(forms.ModelForm):
+    class Meta:
+        model = OpeningHour
+        fields = ['day', 'from_hour','to_hour','is_closed']
